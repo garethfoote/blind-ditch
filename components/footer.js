d6038464c0989a894eaf4def988eab76545d602b
garethfoote <gareth.foote@gmail.com>
2020-12-30 14:39:33 +0000

    Updated footer and projects page

--- components/footer.js
+++ components/footer.js
@@ -3,13 +3,16 @@ import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center container bg-black h-56 w-full">
-      <div className="h-14">
-        <img
-          alt="Blind Ditch logo in black"
-          className="h-full w-auto"
-          src="/logo-white.png"
-        />
+    <footer className="bg-black h-56 w-full">
+      <div className="flex h-full justify-center items-center container mx-auto">
+        <div className="h-12">
+          <img
+            alt="Blind Ditch logo in black"
+            className="h-full w-auto"
+            src="/logo-white.png"
+          />
+        </div>
+        <div className="flex-grow"></div>
       </div>
     </footer>
   );
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,21 +1,31 @@
 import Layout from "../../components/layout";
 import Container from "../../components/container";
 import { ProjectList } from "../../components/ProjectList";
+import { Intro } from "../../components/Intro";
 import Head from "next/head";
 
 import { getAllProjects } from "../../lib/api";
+import { SectionTitle } from "../../components/SectionTitle";
+import { Text } from "../../components/Text";
 
 export default function Index({ allProjects: { nodes } }) {
   return (
     <>
       <Layout>
+        <Intro />
         <Head>
           <title>Projects</title>
         </Head>
-        <Container>
-          <h1>Work</h1>
+        <div className="container mx-auto mb-xl p-5">
+          <SectionTitle>Work</SectionTitle>
+          <div className="text-center px-5 max-w-xl mx-auto">
+            <Text
+              isCentred={true}
+              content="Short description or reminder of the type of work you ususally embark on."
+            />
+          </div>
           <ProjectList key={20} projects={nodes} />
-        </Container>
+        </div>
       </Layout>
     </>
   );
