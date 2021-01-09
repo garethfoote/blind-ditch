63bcc2865657d557bc1cd6478899fca63c2c51c4
garethfoote <gareth.foote@gmail.com>
2021-01-09 11:43:50 +0000

    More spacing in header

--- components/HomeHeader/HomeHeader.js
+++ components/HomeHeader/HomeHeader.js
@@ -7,7 +7,7 @@ import classnames from "classnames";
 export const HomeHeader = ({ intro }) => {
   return (
     <div className={classnames(styles.headerCols)}>
-      <div className="sm:mx-0 pb-lg sm:pb-0 sm:pr-md">
+      <div className="sm:mx-0 pb-lg sm:pb-0 sm:pr-lg">
         <div
           className="text-base leading-7 sm:text-md sm:leading-8 text-back-blue"
           dangerouslySetInnerHTML={{ __html: intro }}
