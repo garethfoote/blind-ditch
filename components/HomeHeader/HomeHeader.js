09fdd174d9d86e88a896dfcb964affcec802eea5
garethfoote <gareth.foote@gmail.com>
2021-01-09 12:35:13 +0000

    Read more link on homepage

--- components/HomeHeader/HomeHeader.js
+++ components/HomeHeader/HomeHeader.js
@@ -1,17 +1,20 @@
-import { Text } from "../../components/Text";
-import { PullQuote } from "../../components/PullQuote";
+import Link from "next/link";
 import { HighlightBox } from "../../components/HighlightBox";
 import styles from "./HomeHeader.module.css";
 import classnames from "classnames";
 
 export const HomeHeader = ({ intro }) => {
+  const readMore = '<a class="dash-underline" href="/about">Read more</a>  →';
   return (
     <div className={classnames(styles.headerCols)}>
       <div className="sm:mx-0 pb-lg sm:pb-0 sm:pr-lg">
-        <div
-          className="text-base leading-7 sm:text-md sm:leading-8 text-back-blue"
-          dangerouslySetInnerHTML={{ __html: intro }}
-        />
+        <div className="text-base leading-7 sm:text-md sm:leading-8 text-back-blue">
+          <div className="mb-sm" dangerouslySetInnerHTML={{ __html: intro }} />
+          <Link href="/page/about">
+            <a className="dash-underline">Read More</a>
+          </Link>{" "}
+          →
+        </div>
       </div>
       <div className="sm:mx-0">
         <HighlightBox
