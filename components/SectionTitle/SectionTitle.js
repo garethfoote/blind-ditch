0a9595f641c2e9fbf3bdb2b878be78d0aa77ccd6
garethfoote <gareth.foote@gmail.com>
2021-01-15 17:44:32 +0000

    Modified section title

--- components/SectionTitle/SectionTitle.js
+++ components/SectionTitle/SectionTitle.js
@@ -1,9 +1,12 @@
 import styles from "./SectionTitle.module.css";
+import classnames from "classnames";
 
 export const SectionTitle = ({ children, text }) => (
   <div className="text-center px-5 max-w-xl mx-auto my-lg sm:my-xl">
     <div className="text-center">
-      <h2 className={styles.title}>{text || children}</h2>
+      <h2 className={classnames(styles.title, "title-underline leading-10")}>
+        {text || children}
+      </h2>
     </div>
   </div>
 );
