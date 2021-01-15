6967628d1f18f2b96e817abb110f5311b823f82e
garethfoote <gareth.foote@gmail.com>
2021-01-15 23:00:52 +0000

    Logo positioning:

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -16,7 +16,9 @@ export default function Index({ allProjects: { nodes } }) {
           <title>Blind Ditch</title>
         </Head>
         <Nav />
-        <Logo />
+        <div className="flex items-center justify-center h-20 mb-sm md:h-28 md:mb-md lg:h-24 lg:mb-sm">
+          <Logo />
+        </div>
 
         <div className="container mx-auto mb-xl">
           <SectionTitle>Work</SectionTitle>
