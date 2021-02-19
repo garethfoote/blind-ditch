b271ce66cc05c57853f2137d9aea95bc880d2cae
garethfoote <gareth.foote@gmail.com>
2021-02-19 16:08:39 +0000

    minor margin tweak:

--- components/ProjectPageTitle/ProjectPageTitle.js
+++ components/ProjectPageTitle/ProjectPageTitle.js
@@ -8,7 +8,7 @@ import styles from "./ProjectPageTitle.module.css";
 export const ProjectPageTitle = ({ title, date }) => {
   return (
     <div className="mx-auto px-5 md:px-10 lg:px-5 max-w-2xl text-center pt-8">
-      <h2 className="uppercase inline tracking-widest text-xl sm:text-2xl leading-10 sm:leading-tight">
+      <h2 className="mb-sm uppercase inline tracking-widest text-xl sm:text-2xl leading-10 sm:leading-tight">
         {title}
       </h2>
       <h3 className={classnames("font-accent text-lg")}>({date})</h3>
