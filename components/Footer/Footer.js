46aa40e856764649abfeafc2ae8f9719ff842a9b
garethfoote <gareth.foote@gmail.com>
2021-02-26 22:27:48 +0000

    Hover underline for project titles

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,23 +6,21 @@ import globalData from "../../data/global-manifest.json";
 export const Footer = () => {
   return (
     <footer className={classnames(styles.footer, "bg-black w-full")}>
-      <div className="flex h-full justify-center items-center container mx-auto">
-        <div className="flex-grow w-1/2">
-          1
-          <div className="h-auto px-md w-48">
-            <img
-              alt="Blind Ditch logo in black"
-              className="h-full w-auto"
-              src="/logo-white.png"
-            />
-          </div>
-        </div>
-        <div className="flex-grow w-1/2">
+      <div className="h-full justify-center items-center container mx-auto">
+        
+        <div className="text-center py-lg">
           {globalData.footer.map(({ node }) => (
             <Link as={`/page/${node.slug}`} href="/page/[slug]">
               <a className="text-offwhite">{node.title}</a>
             </Link>
           ))}
+        </div>
+        <div className="py-lg h-auto mx-auto px-md w-48">
+          <img
+            alt="Blind Ditch logo in black"
+            className="h-auto w-full"
+            src="/logo-white.png"
+          />
         </div>
       </div>
     </footer>
--- components/ProjectList/ProjectListItem.js
+++ components/ProjectList/ProjectListItem.js
@@ -55,7 +55,7 @@ export const ProjectListItem = ({
           {yearStart}
         </time>
         <Link as={`/projects/${slug}`} href="/projects/[slug]">
-          <a className="hover:underline text-sm sm:text-md md:text-lg uppercase leading-6 tracking-wide pl-lg sm:pl-xl whitespace-nowrap">
+          <a className="title-underline-hover text-sm sm:text-md md:text-lg uppercase leading-6 tracking-wide ml-lg sm:ml-xl whitespace-nowrap">
             {title}
           </a>
         </Link>
