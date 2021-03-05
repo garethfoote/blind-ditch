0daa2b1634ca5d32a580fb53ebb870a4d1e974eb
garethfoote <gareth.foote@gmail.com>
2021-03-05 20:05:04 +0000

    Section title spacing on mobile

--- components/SectionTitle/SectionTitle.js
+++ components/SectionTitle/SectionTitle.js
@@ -1,5 +1,5 @@
 export const SectionTitle = ({ children, text }) => (
-  <div className="text-center px-5 max-w-xl mx-auto my-lg sm:my-xl">
+  <div className="text-center px-5 max-w-xl mx-auto mt-xl mb-lg sm:my-xl">
     <div className="text-center">
       <h2 className="text-md sm:text-xl title-underline inline uppercase leading-10 sm:leading-14 tracking-wider">
         {text || children}
