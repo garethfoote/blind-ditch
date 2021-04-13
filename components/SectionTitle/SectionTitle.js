8822b2e438157a957545ea855600160dacc3480f
garethfoote <gareth.foote@gmail.com>
2021-04-13 19:03:33 +0100

    Sizing of title

--- components/SectionTitle/SectionTitle.js
+++ components/SectionTitle/SectionTitle.js
@@ -1,9 +1,7 @@
 export const SectionTitle = ({ children, text }) => (
-  <div className="text-center px-5 max-w-2xl mx-auto mt-xl mb-lg sm:my-xl">
-    <div className="text-center">
-      <h2 className="text-md sm:text-xl title-underline inline uppercase leading-10 sm:leading-14 tracking-wider">
-        {text || children}
-      </h2>
-    </div>
+  <div className="text-center px-5 max-w-2xl mx-auto mt-lg mb-md">
+    <h2 className="text-md sm:text-xl title-underline inline uppercase leading-10 sm:leading-14 tracking-wider">
+      {text || children}
+    </h2>
   </div>
 );
