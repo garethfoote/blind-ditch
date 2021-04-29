77163569ea212511db542a180f17791a55fd7062
garethfoote <gareth.foote@gmail.com>
2021-04-29 16:24:04 +0100

    Title resizing

--- components/ProjectPageTitle/ProjectPageTitle.js
+++ components/ProjectPageTitle/ProjectPageTitle.js
@@ -3,10 +3,10 @@ import classnames from "classnames";
 export const ProjectPageTitle = ({ title, date }) => {
   return (
     <div className="mx-auto px-5 md:px-10 lg:px-5 max-w-2xl text-center pt-8">
-      <h2 className="mb-sm uppercase inline tracking-widest text-xl sm:text-2xl leading-10 sm:leading-tight">
+      <h2 className="mb-sm uppercase inline tracking-widest text-lg sm:text-xl leading-8 sm:leading-tight">
         {title}
       </h2>
-      <h3 className={classnames("font-accent text-lg")}>({date})</h3>
+      <h3 className={classnames("font-accent text-sm sm:text-md")}>({date})</h3>
     </div>
   );
 };
