414613301c07b262df0cd8988f881372b5377b57
garethfoote <gareth.foote@gmail.com>
2021-04-29 16:50:09 +0100

    Made document header its own component

---
+++ components/Documents/DocumentHeader.js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+
+export const DocumentHeader = () => {
+  const router = useRouter();
+
+  return (
+    <div className="flex gap-4 py-md justify-between">
+      <div className="flex-none w-24 px-sm">
+        <button className="h-4 button-shadow">
+          <span
+            onClick={() => router.back()}
+            className="text-xs text-blue relative z-10 title-accent p-2 bg-white border border-blue border-solid"
+          >
+            ← Back
+          </span>
+        </button>
+      </div>
+
+      <div className="flex gap-4 max-w-2xl pr-md">
+        <p>
+          A collection of documents and media produced over the course of our
+          20+ years working with x, y & z.
+        </p>
+        <div>
+          <svg
+            width="70"
+            height="96"
+            viewBox="0 0 70 96"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M69 95H1V1H49.5714L69 21.4V95Z"
+              stroke="#1F1F1F"
+              strokeWidth="2"
+            />
+            <path d="M49 22V1L69 22H49Z" fill="#1F1F1F" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
