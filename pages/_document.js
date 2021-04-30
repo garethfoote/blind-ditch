2070817da474414f714da241bc908c651e60afc7
garethfoote <gareth.foote@gmail.com>
2021-04-30 14:33:14 +0100

    Matomo anlytics testing

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,16 @@ export default class MyDocument extends Document {
             dangerouslySetInnerHTML={{ __html: mediaStyles }}
             type="text/css"
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              var _mtm = window._mtm = window._mtm || [];
+              _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.src='http://bd.bowt.club/wp-content/plugins/matomo/app/../../../uploads/matomo/container_FvJyR9Pa.js'; s.parentNode.insertBefore(g,s);
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
