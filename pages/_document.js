43269353dda81644f4804c265aaa29f98d7e797a
garethfoote <gareth.foote@gmail.com>
2021-05-05 22:25:41 +0100

    Tracking code update

--- pages/_document.js
+++ pages/_document.js
@@ -13,10 +13,10 @@ export default class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              var _mtm = window._mtm = window._mtm || [];
-              _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.type='text/javascript'; g.async=true; g.src='https://bd.bowt.club/wp-content/plugins/matomo/app/../../../uploads/matomo/container_FvJyR9Pa.js'; s.parentNode.insertBefore(g,s);
+              
+              var _paq = window._paq = window._paq || [];
+              _paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);_paq.push(['alwaysUseSendBeacon']);_paq.push(['setTrackerUrl', "\/\/bd.bowt.club\/wp-content\/plugins\/matomo\/app\/matomo.php"]);_paq.push(['setSiteId', '1']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.src="\/\/bd.bowt.club\/wp-content\/uploads\/matomo\/matomo.js"; s.parentNode.insertBefore(g,s);
               `,
             }}
           />
