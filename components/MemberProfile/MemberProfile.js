bba0bfa4236666e10bd24af5c308d29912081a34
garethfoote <gareth.foote@gmail.com>
2021-05-20 14:33:14 +0100

    Modified dashed lines

--- components/MemberProfile/MemberProfile.js
+++ components/MemberProfile/MemberProfile.js
@@ -11,62 +11,65 @@ export const MemberProfile = ({
   projects,
 }) => {
   return (
-    <div className="border-b border-black h-full">
-      <div className="relative w-full h-80 mb-4 image-border">
-        {image && (
-          <Image
-            className="image-loading"
-            alt={image.altText || name}
-            src={image.sourceUrl}
-            layout="fill"
-            objectFit="cover"
-          />
-        )}
-      </div>
-      <p className="text-lg mb-4">{name}</p>
-      <div className="mb-4 flex uppercase font-accent text-xs leading-6 tracking-wide">
-        <div className="w-48">
-          <dl>
-            <dt className="text-blue">Dates</dt>
-            <dd>{date}</dd>
-          </dl>
+    <>
+      <div className="border-b border-offwhite h-full">
+        <div className="relative w-full h-80 mb-4 image-border">
+          {image && (
+            <Image
+              className="image-loading"
+              alt={image.altText || name}
+              src={image.sourceUrl}
+              layout="fill"
+              objectFit="cover"
+            />
+          )}
         </div>
-        {projects && (
-          <div className="flex-grow">
+        <p className="text-lg mb-4">{name}</p>
+        <div className="mb-4 flex uppercase font-accent text-xs leading-6 tracking-wide">
+          <div className="w-48">
             <dl>
-              <dt className="text-blue">Projects</dt>
-              {projects.map(
-                (project, idx) =>
-                  project && (
-                    <dd key={idx}>
-                      <span className="inline bg-yellow">
-                        <Link
-                          as={`/projects/${project.slug}`}
-                          href="/projects/[slug]"
-                        >
-                          <a>{project.title}</a>
-                        </Link>
-                      </span>
-                      {idx != projects.length - 1 && ", "}
-                    </dd>
-                  )
-              )}
+              <dt className="text-blue">Dates</dt>
+              <dd>{date}</dd>
+            </dl>
+          </div>
+          {projects && (
+            <div className="flex-grow">
+              <dl>
+                <dt className="text-blue">Projects</dt>
+                {projects.map(
+                  (project, idx) =>
+                    project && (
+                      <dd key={idx}>
+                        <span className="inline bg-yellow">
+                          <Link
+                            as={`/projects/${project.slug}`}
+                            href="/projects/[slug]"
+                          >
+                            <a>{project.title}</a>
+                          </Link>
+                        </span>
+                        {idx != projects.length - 1 && ", "}
+                      </dd>
+                    )
+                )}
+              </dl>
+            </div>
+          )}
+        </div>
+        {role && (
+          <div className="mb-4 uppercase font-accent text-xs leading-6 tracking-wide">
+            <dl>
+              <dt className="text-blue">Role</dt>
+              <dd>{role}</dd>
             </dl>
           </div>
         )}
-      </div>
-      {role && (
-        <div className="mb-4 uppercase font-accent text-xs leading-6 tracking-wide">
-          <dl>
-            <dt className="text-blue">Role</dt>
-            <dd>{role}</dd>
-          </dl>
-        </div>
-      )}
 
-      <div className="border-t border-black pt-md">
-        <Text content={profile} />
+        <div className="border-t border-black pt-md">
+          <Text content={profile} />
+        </div>
       </div>
-    </div>
+      <hr className="page-break-thin mb-md" />
+    </>
   );
 };
