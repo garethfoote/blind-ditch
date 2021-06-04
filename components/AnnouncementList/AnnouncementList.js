e1b9037aeff982a98ca19962baea4555bd9e3640
garethfoote <gareth.foote@gmail.com>
2021-06-04 11:31:57 +0100

    Adding message to final announcement

--- components/AnnouncementList/AnnouncementList.js
+++ components/AnnouncementList/AnnouncementList.js
@@ -87,7 +87,9 @@ export const AnnouncementList = ({ announcements }) => {
               <div className="relative w-36 sm:w-40 h-60 sm:h-48 bg-offwhite bg-topography bg-50"></div>
               <div className="w-44 sm:w-64 h-60 sm:h-48 p-4 bg-offwhite">
                 <div className="font-accent flex border-b border-black pb-2">
-                  <h2 className="flex-grow text-xs uppercase leading-4 sm:leading-6"></h2>
+                  <h2 className="flex-grow text-xs uppercase leading-4 sm:leading-6">
+                    No more announcements
+                  </h2>
                 </div>
                 {/* <p className="text-sm sm:text-base mt-4">End</p> */}
               </div>
