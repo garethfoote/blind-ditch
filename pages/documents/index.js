fc2d617f5eb77c4c51c583dc3dbfa70b8e08e979
garethfoote <gareth.foote@gmail.com>
2021-08-24 11:50:20 +0100

    Adding missing main el

--- pages/documents/index.js
+++ pages/documents/index.js
@@ -17,10 +17,10 @@ export default function Index({ allDocs, docsPageFields }) {
             content={docsPageFields.documentsPageIntroText}
           />
         </Head>
-        <div className="px-xs md:px-lg mx-auto mb-xl">
+        <main className="px-xs md:px-lg mx-auto mb-xl">
           <DocumentHeader title={docsPageFields.documentsPageIntroText} />
           <Documents documents={allDocs} />
-        </div>
+        </main>
       </Layout>
     </>
   );
