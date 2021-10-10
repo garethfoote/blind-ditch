4dd8b34ccbdc424daf818b1b3c879a8a4d3c5cab
garethfoote <gareth.foote@gmail.com>
2021-10-10 15:15:18 +0100

    Hiding video until in view

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,10 +1,35 @@
 import classnames from "classnames";
 import styles from "./Footer.module.css";
+import { useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
 // import globalData from "../../data/global-manifest.json";
 
+export default function useOnScreen(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  const observer = new IntersectionObserver(([entry]) =>
+    setIntersecting(entry.isIntersecting)
+  );
+
+  useEffect(() => {
+    observer.observe(ref.current);
+    // Remove the observer as soon as the component is unmounted
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return isIntersecting;
+}
+
 export const Footer = () => {
+  // const footerRef = useRef();
+  const [footerRef, inView, entry] = useInView({
+    threshold: 0,
+  });
+
   return (
-    <footer className={classnames(styles.footer, "w-full")}>
+    <footer ref={footerRef} className={classnames(styles.footer, "w-full")}>
       <div className="flex h-full justify-center items-center mx-0 sm:mx-xl">
         <div className="flex-1">
           <div className="pl-md max-w-xs">
@@ -15,7 +40,12 @@ export const Footer = () => {
             />
           </div>
         </div>
-        <div className="flex-1 max-h-56">
+        <div
+          className={classnames("flex-1 max-h-56", {
+            ["hidden"]: !inView,
+            ["block"]: inView,
+          })}
+        >
           <video autoPlay loop muted>
             <source src="/Blind_Ditch_anim.mp4" type="video/mp4" />
           </video>
